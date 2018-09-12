0d2c8de1fde5c6e482ea2b8784a669de8b851491
Nathan <nathanjwtx@nathanjwtx.io>
2018-09-12 08:15:43 -0500

    Completed second_index from Elementary

---
+++ second_index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function secondIndex(text, symbol) {
+    // returns the second index of a symbol in a given text
+
+    var res = text.match(symbol)
+    const first = res !== null ? res['index'] : undefined
+    const sub = first !== undefined ? text.substring(res['index']+1).indexOf(symbol) : -1
+    return sub !== -1 ? text.substring(res['index']+1).indexOf(symbol) + res['index'] + 1 : undefined
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    console.log('Example')
+    console.log(secondIndex("simple", " "))
+    // console.log(secondIndex("simple simon is simple", "i"))
+
+    // These "asserts" are used for self-checking and not for an auto-testing
+    // assert.equal(secondIndex("sims", "s"), 3)
+    // assert.equal(secondIndex("find the river", "e"), 12)
+    // assert.equal(secondIndex("hi", " "), undefined)
+    // assert.equal(secondIndex("hi mayor", " "), undefined)
+    // assert.equal(secondIndex("hi mr Mayor", " "), 5)
+    // console.log("You are awesome! All tests are done! Go Check it!");
+}
